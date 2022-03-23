0dcff9858e82dfffce0fe95cc7ea2125cc88190d
Ruby Adkins <radkins22@students.stab.org>
2022-03-23 00:03:14 -0400

    add public folder

---
+++ public/client.js
@@ -0,0 +1,26 @@
+const socket = io();
+
+//receive all chats when connected
+socket.on('allChats', (chats) => {
+	console.log(chats);
+
+	//display chat array
+	for (let i = 0; i < chats.length; i++) {
+		$("#chats").append("<li>" + chats[i].chat + "</li>");
+	}
+
+});
+
+//send new chats when button is clicked
+$("button").click((event) => {
+	//prevent default refresh page
+	event.preventDefault()
+
+	//send chat from input to server
+	socket.emit("newChat", $("input").val());
+})
+
+//receive new chats and append them
+socket.on("newChattoUsers", (msg) => {
+	$("#chats").append("<li>" + msg + "</li>");
+});
