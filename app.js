d9fd298b2252e87b969c8f112602dfaf4fdd6d1e
Ruby Adkins <radkins22@students.stab.org>
2022-04-05 09:53:00 -0400

    add mariadb

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const app = express()
 const server = http.createServer(app)
 const { Server } = require("socket.io");
 const io = new Server(server);
-const sqlite3 = require('sqlite3')
 const bodyParser = require('body-parser')
+const mysql = require("mysql2");
+
+//connect to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "me",
+  password: "password",
+  database: "pbnj",
+  port: 1433
+});
 
 //setup app
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -16,48 +25,39 @@ var engines = require('consolidate')
 app.engine('html', engines.hogan)
 app.set('views', __dirname + '/views')
 
-//connect to database
-let db = new sqlite3.Database('./chatlog.db', sqlite3.OPEN_READWRITE, err => {
-  console.log(err)
-})
-
-//create table *comment out because only run once!*
-//db.run("CREATE TABLE chatlog(chat TEXT)");
-
 //display the home page
 app.get('/', (req, res) => {
   res.render('index.html', {})
 })
 
 //display chat page
-app.post('/chat', (req, res) => {
-    res.render('chat.html', {})
+app.get('/chat', (req, res) => {
+  res.render('chat.html', {})
 })
 
 //connect new user
 io.on('connection', socket => {
   //send new user all chat messages from database
-  db.all('SELECT * FROM chatlog', function (err, result) {
+  connection.query("SELECT * FROM chathistory;", (err, res) => {
     //print error
     if (err) console.log(err)
 
     //send new chat to all connected users
-    socket.emit('allChats', result)
+    socket.emit('allChats', res)
   })
 
   //listen for new messages
   socket.on('newChat', newChat => {
     //add chat to database
-    db.run('INSERT INTO chatlog VALUES (?)', newChat)
-
+    connection.query('INSERT INTO chathistory(chat, user_id) VALUES (?, ?);', [newChat, 1], (err) => {
+      if (err) console.log(err);
     //send out new chat to connected users
     io.emit('newChattoUsers', newChat)
   })
+})
 })
 
 //listen to server
 server.listen(3000, () => {
   console.log('listening on *:3000')
 })
-
-console.log('Web Server is listening at port ' + (process.env.port || 3000))
