3a98c7ed5aa8060473b804ba8a02c36d79fe775c
Ruby Adkins <radkins22@students.stab.org>
2022-04-12 22:29:14 -0400

    send user id

--- app.js
+++ app.js
@@ -5,28 +5,27 @@ const bcrypt = require('bcrypt')
 const http = require('http')
 const app = express()
 const server = http.createServer(app)
-const {
-    Server
-} = require('socket.io')
+const { Server } = require('socket.io')
 const io = new Server(server)
 const bodyParser = require('body-parser')
 const mysql = require('mysql2')
 const config = require('dotenv').config()
 
-
 //connect to database
 const connection = mysql.createConnection({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.PORT
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
+  port: process.env.PORT
 })
 
 //setup app
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}))
+  })
+)
 app.use(bodyParser.json())
 app.use(express.static('public'))
 var engines = require('consolidate')
@@ -35,96 +34,116 @@ app.set('views', __dirname + '/views')
 
 //display the home page
 app.get('/', (req, res) => {
-    res.render('index.html', {})
+  res.render('index.html', {})
 })
 
 //display chat page
 app.get('/chat', (req, res) => {
-    res.render('chat.html', {})
+  res.render('chat.html', {})
 })
 
 //display authentication page
-app.get('/Auth', function(req, res) {
-    res.render('Auth.html', {})
+app.get('/Auth', function (req, res) {
+  res.render('Auth.html', {})
 })
 
 //display main menu
-app.get('/ButtonMenu', function(req, res) {
-    res.render('buttonmenu.html', {})
+app.get('/ButtonMenu', function (req, res) {
+  res.render('buttonmenu.html', {})
 })
-
-//login
-
 
 //connect new user
 io.on('connection', socket => {
-    //send new user all chat messages from database
-    connection.query('SELECT * FROM chathistory;', (err, res) => {
-        //print error
+  //send new user all chat messages from database
+  connection.query('SELECT * FROM chathistory;', (err, res) => {
+    //print error
+    if (err) console.log(err)
+
+    //send new chat to all connected users
+    socket.emit('allChats', res)
+  })
+
+  //login
+  socket.on('login', (username, passwordd) => {
+    console.log(username, passwordd)
+    let UN, password, hashedPassword
+    UN = username + ''
+    password = passwordd
+    connection.query(
+      'SELECT hashword FROM userinfo WHERE username = (?)',
+      [UN],
+      (err, result) => {
+        if (err) throw err
+        console.log(result)
+        if (result.length == 0) {
+          // res.render('index.html', {})
+          socket.emit('loginStatus', false)
+        } else {
+          hashedPassword = result[0]['hashword']
+          //where the guessed password is compared
+          bcrypt.compare(password, hashedPassword, function (err, isMatch) {
+            if (err) console.log(err)
+            console.log(isMatch)
+
+            // Comparing the original password to
+            // encrypted password
+            if (isMatch) {
+              console.log('Encrypted password is: ', password)
+              console.log('Decrypted password is: ', hashedPassword)
+              // socket.emit("alert", (socket) => {})
+
+              //send userid back for session storage
+              //match username to userid
+              connection.query(
+                'SELECT userid FROM userinfo WHERE username=(?);',
+                [UN],
+                (err, res) => {
+                  //print error
+                  if (err) console.log(err)
+
+                  console.log(res[0].userid)
+
+                  //send id to client
+                  socket.emit('sessionStorage', res[0].userid, (response) => {
+                    console.log('asdjfs', response)
+                    //send true status
+                    socket.emit('loginStatus', true)
+                  })
+                }
+              )
+            }
+
+            if (!isMatch) {
+              // If password doesn't match the following
+              // message will be sent
+              console.log(hashedPassword + ' is not encryption of ' + password)
+              socket.emit('loginStatus', false)
+            }
+          })
+        }
+      }
+    )
+  })
+
+  //listen for new messages
+  socket.on('newChat', (newChat, userid) => {
+    console.log(userid)
+    //add chat to database
+    connection.query(
+      'INSERT INTO chathistory(chat, userid) VALUES (?, ?);',
+      [newChat, userid],
+      err => {
         if (err) console.log(err)
 
-        //send new chat to all connected users
-        socket.emit('allChats', res)
-    })
-
-
-    socket.on('login', (username, passwordd) => {
-        console.log(username, passwordd)
-        let UN, password, hashedPassword
-        UN = username + ''
-        password = passwordd
-        connection.query(
-            'SELECT hashword FROM userinfo WHERE username = (?)', [UN],
-            (err, result) => {
-                if (err) throw err
-                console.log(result)
-                if (result.length == 0) {
-                    // res.render('index.html', {})
-                    socket.emit("loginStatus", false)
-                } else {
-                    hashedPassword = result[0]['hashword']
-                        //where the guessed password is compared
-                    bcrypt.compare(password, hashedPassword, function(err, isMatch) {
-                        if (err) console.log(err)
-                        console.log(isMatch)
-
-                        // Comparing the original password to
-                        // encrypted password
-                        if (isMatch) {
-                            console.log('Encrypted password is: ', password)
-                            console.log('Decrypted password is: ', hashedPassword)
-                            socket.emit("loginStatus", true)
-                            // socket.emit("alert", (socket) => {})
-                        }
-
-                        if (!isMatch) {
-                            // If password doesn't match the following
-                            // message will be sent
-                            console.log(hashedPassword + ' is not encryption of ' + password)
-                            socket.emit("loginStatus", false)
-                        }
-                    })
-                }
-            }
-        )
-    })
-
-
-    //listen for new messages
-    socket.on('newChat', newChat => {
-        //add chat to database
-        connection.query(
-            'INSERT INTO chathistory(chat, userid) VALUES (?, ?);', [newChat, 1],
-            err => {
-                if (err) console.log(err)
-                    //send out new chat to connected users
-                io.emit('newChattoUsers', newChat)
-            }
-        )
-    })
+        console.log(userid)
+        //send out new chat to connected users
+        io.emit('newChattoUsers', newChat)
+      }
+    )
+  })
 })
 
 //listen to server
 server.listen(3000, () => {
-    console.log('listening on *:3000')
+  console.log('listening on *:3000')
 })
